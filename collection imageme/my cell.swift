9da57cb70380c16bbe856f5a53603d645bea89a5
Obed D <obed@obeds-macbook-pro-184.local>
2018-11-22 20:10:18 +0700

    Initial Commit

---
+++ collection imageme/my cell.swift
@@ -0,0 +1,14 @@
+//
+//  my cell.swift
+//  collection imageme
+//
+//  Created by Obed RPL D on 22/11/18.
+//  Copyright © 2018 Syams.org. All rights reserved.
+//
+
+import UIKit
+
+class my_cell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+}
